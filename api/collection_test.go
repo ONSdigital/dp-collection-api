46b7835b93c9e5471b4155f6153c6bed41a585cb
CarlHembrough <carlhuk@gmail.com>
2021-06-18 09:47:37 +0100

    Add test case for an empty orderBy value

--- api/collection_test.go
+++ api/collection_test.go
@@ -86,7 +86,7 @@ func TestGetCollections(t *testing.T) {
 	})
 }
 
-func TestGetCollections_orderBy(t *testing.T) {
+func TestGetCollections_orderByPublishDate(t *testing.T) {
 
 	offset := 0
 	limit := 1
@@ -119,17 +119,54 @@ func TestGetCollections_orderBy(t *testing.T) {
 			api := api.Setup(context.Background(), mux.NewRouter(), paginator, collectionStore)
 			api.GetCollectionsHandler(w, r)
 
-			Convey("Then the paginator is called to extract pagination parameters", func() {
-				So(len(paginator.ReadPaginationParametersCalls()), ShouldEqual, 1)
-				So(paginator.ReadPaginationParametersCalls()[0].R, ShouldEqual, r)
-			})
-
 			Convey("Then the collection store is called with the expected orderBy value", func() {
 				So(len(collectionStore.GetCollectionsCalls()), ShouldEqual, 1)
 				getCollectionsCall := collectionStore.GetCollectionsCalls()[0]
 				So(getCollectionsCall.Limit, ShouldEqual, limit)
 				So(getCollectionsCall.Offset, ShouldEqual, offset)
 				So(getCollectionsCall.OrderBy, ShouldEqual, collections.OrderByPublishDate)
+			})
+		})
+	})
+}
+
+func TestGetCollections_emptyOrderBy(t *testing.T) {
+
+	offset := 0
+	limit := 1
+	totalCount := 3
+
+	Convey("Given a request to GET collections with an empty order_by value", t, func() {
+
+		paginator := &mock.PaginatorMock{
+			ReadPaginationParametersFunc: func(r *http.Request) (int, int, error) {
+				return offset, limit, nil
+			},
+		}
+
+		collectionStore := &mock.CollectionStoreMock{
+			GetCollectionsFunc: func(ctx context.Context, offset int, limit int, orderBy collections.OrderBy) ([]models.Collection, int, error) {
+				return []models.Collection{{
+					ID:          "123",
+					Name:        "collection 1",
+					PublishDate: time.Time{},
+					LastUpdated: time.Time{},
+				}}, totalCount, nil
+			},
+		}
+
+		r := httptest.NewRequest("GET", "http://localhost:26000/collections?order_by=", nil)
+		w := httptest.NewRecorder()
+
+		Convey("When the request is sent to the API", func() {
+
+			api := api.Setup(context.Background(), mux.NewRouter(), paginator, collectionStore)
+			api.GetCollectionsHandler(w, r)
+
+			Convey("Then the collection store is called with the expected orderBy value", func() {
+				So(len(collectionStore.GetCollectionsCalls()), ShouldEqual, 1)
+				getCollectionsCall := collectionStore.GetCollectionsCalls()[0]
+				So(getCollectionsCall.OrderBy, ShouldEqual, collections.OrderByDefault)
 			})
 		})
 	})
