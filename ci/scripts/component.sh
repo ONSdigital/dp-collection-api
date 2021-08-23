7e31c6f56ea1eedc8e9c1cab229e0b8d20a91103
Rafa <rafael.garcia-pendon@ons.gov.uk>
2021-08-23 18:43:57 +0100

    testing newer version of mongo

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -eux
 
 pushd dp-collection-api
-
-  echo "$(</etc/os-release)"
-
+  export MEMONGO_DOWNLOAD_URL=https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian10-4.2.15.tgz
   make test-component
 popd
